d70b6ced0194023fc684d38c2d0c49fabd300c4e
Ruslan Karimov <ru-ka-@mail.ru>
2023-01-30 18:30:49 +0500

    added Task1

--- Task1.java
+++ Task1.java
@@ -1,7 +1,29 @@
 package homework3;
 
+//Задание 1: пусть дан произвольный список целых чисел.
+// Удалить из него четные числа
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Task1 {
-    public static void main (String[] args){
-        System.out.println("Hello!");
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        for (int i = 0; i < 1000; i++) {
+            int someNum = random.nextInt(-1000, 1000);
+            arrayList.add(someNum);
+        }
+
+        int i = 0;
+        while (i < arrayList.size()) {
+            if (arrayList.get(i) % 2 == 0) {
+                arrayList.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.printf(String.valueOf(arrayList));
     }
 }
